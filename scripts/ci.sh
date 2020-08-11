88ad74e3775931f2a64c196d6790f6530db946ab
Yuvraj <yuvraj.yad001@gmail.com>
2020-08-11 11:32:29 +0000

    test

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -20,6 +20,6 @@ echo "Installing kustomize"
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 chmod a+x $pwd/definitions/kustomize
 echo "Kustomize image edit"
-$pwd/definitions/kustomize edit set image nirmata/kyverno:$hash
-$pwd/definitions/kustomize edit set image nirmata/kyvernopre:$hash
+$pwd/definitions/kustomize edit set image evalsocket/kyverno:$hash
+$pwd/definitions/kustomize edit set image evalsocket/kyvernopre:$hash
 $pwd/definitions/kustomize build $pwd/definitions/ > $pwd/definitions/install.yaml
